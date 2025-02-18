ff718479b4d096241738a1d4e591db25671b794f
igor-cavalcante <igorcavalcantealbuquerque@gmail.com>
2025-02-18 08:18:31 -0300

    initial configs

---
+++ src/test/java/com/web1/Sistema_noticias/SistemaNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web1.Sistema_noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
